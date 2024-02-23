a54a4823166188dda129a6f184720a8a0445e291
ndw1117 <ndw1117@g.rit.edu>
2024-02-23 14:31:56 -0500

    Setup using http-assignment-ii

---
+++ src/response.js
@@ -0,0 +1,93 @@
+const fs = require('fs');
+
+const index = fs.readFileSync(`${__dirname}/../client/client.html`);
+const stylesheet = fs.readFileSync(`${__dirname}/../client/style.css`);
+
+const users = {};
+
+const respondJSON = (request, response, status, statusMessage, object) => {
+  response.writeHead(status, { 'Content-Type': 'application/json', 'Status-Message': statusMessage });
+  response.write(JSON.stringify(object));
+  response.end();
+};
+
+// For requests that only want the head
+const respondJSONMeta = (request, response, status, statusMessage) => {
+  response.writeHead(status, { 'Content-Type': 'application/json', 'Status-Message': statusMessage });
+  response.end();
+};
+
+const addUser = (request, response, body) => {
+  // default json message, displayed when missing field(s)
+  const responseJSON = {
+    message: 'Name and age are both required.',
+  };
+
+  // check to make sure we have both fields
+  if (!body.name || !body.age) {
+    responseJSON.id = 'addUserMissingParams';
+    return respondJSON(request, response, 400, 'Bad Request', responseJSON);
+  }
+
+  // If both fields present:
+
+  let responseCode = 204;
+
+  // If the user doesn't exist yet
+  if (!users[body.name]) {
+    responseCode = 201;
+    users[body.name] = {};
+  }
+
+  users[body.name].name = body.name;
+  users[body.name].age = body.age;
+
+  if (responseCode === 201) {
+    responseJSON.message = 'Created Successfully';
+    return respondJSON(request, response, responseCode, 'Created', responseJSON);
+  }
+
+  // Else response code is 204, only head returned
+  return respondJSONMeta(request, response, responseCode, 'Updated (No Content)');
+};
+
+const notReal = (request, response) => {
+  if (request.method === 'HEAD') {
+    return respondJSONMeta(request, response, 404, 'Not Found');
+  }
+
+  const responseJSON = {
+    message: 'The page you are looking for was not found.',
+    id: 'notFound',
+  };
+
+  return respondJSON(request, response, 404, 'Not Found', responseJSON);
+};
+
+const getUsers = (request, response) => {
+  if (request.method === 'HEAD') {
+    return respondJSONMeta(request, response, 200, 'Success');
+  }
+
+  const responseJSON = { users };
+
+  return respondJSON(request, response, 200, 'Success', responseJSON);
+};
+
+const getStylesheet = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/css' });
+  response.write(stylesheet);
+  response.end();
+};
+
+const getIndex = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(index);
+  response.end();
+};
+
+module.exports.getIndex = getIndex;
+module.exports.getStylesheet = getStylesheet;
+module.exports.getUsers = getUsers;
+module.exports.notReal = notReal;
+module.exports.addUser = addUser;
---
+++ src/server.js
@@ -0,0 +1,58 @@
+const http = require('http');
+const url = require('url');
+const query = require('querystring');
+
+const handler = require('./response.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const urlStruct = {
+  '/': handler.getIndex,
+  '/style.css': handler.getStylesheet,
+  '/getUsers': handler.getUsers,
+  '/notReal': handler.notReal,
+  '/addUser': handler.addUser,
+};
+
+const parseBody = (request, response, handlerFunction) => {
+  // Array to store request pieces
+  const body = [];
+
+  request.on('error', (err) => {
+    console.dir(err);
+    response.statusCode = 400;
+    response.end();
+  });
+
+  // Occurs when a piece of the body arrives
+  request.on('data', (chunk) => {
+    body.push(chunk);
+  });
+
+  request.on('end', () => {
+    const bodyString = Buffer.concat(body).toString();
+    const bodyParams = query.parse(bodyString);
+
+    handlerFunction(request, response, bodyParams);
+  });
+};
+
+const onRequest = (request, response) => {
+  const parsedUrl = url.parse(request.url);
+
+  if (parsedUrl.pathname === '/addUser') {
+    return parseBody(request, response, handler.addUser);
+  }
+
+  const handlerFunc = urlStruct[parsedUrl.pathname];
+  if (handlerFunc) {
+    return handlerFunc(request, response);
+  }
+
+  // Else page not found
+  return urlStruct['/notReal'](request, response);
+};
+
+http.createServer(onRequest).listen(port, () => {
+  console.log(`Listening on 127.0.0.1:${port}`);
+});
